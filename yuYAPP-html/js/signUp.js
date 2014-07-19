d27291f0f4942b743af69250bed9f16d9ea2593b
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-07-19 18:52:46 +0530

    SignIn functionality
    Yuapp . signIn Functionality

---
+++ yuYAPP-html/js/signUp.js
@@ -0,0 +1,29 @@
+﻿/*Signup the user*/
+function Signup(SignUpFirstName, SignUpLastName, SignUpUserName, SignUpUserName, SignUpEmail, SignUpPassword, SignUpDOB, SignUpgender) {
+    
+    var userData = {
+        userName: SignUpUserName,
+        email: SignUpEmail,
+        password: SignUpPassword,
+        DOB: SignUpDOB,                         //'11/02/1986'
+        firstName: SignUpFirstName,
+        lastName: SignUpLastName,
+        gender: SignUpgender,
+        securityQuestion: 'New question'
+    };
+    $.ajax({
+        type: "POST",
+        // url: "http://localhost:6269/users/signup",
+        url: "http://174.141.233.6/YuY/users/signup",
+        data: userData,
+        success: function (data) {
+            debugger;
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
