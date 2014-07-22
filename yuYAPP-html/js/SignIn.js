bcfb2b03dc2067dbfaa34f7d6b4fe85353ef7df5
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-07-22 16:56:00 +0530

    login, register, validate e-mail
    implemnted web-services

--- yuYAPP-html/js/SignIn.js
+++ yuYAPP-html/js/SignIn.js
@@ -1,6 +1,5 @@
 ﻿function SignIn(userLoginEmailId, loginPassword) {
-    alert(userLoginEmailId);
-    alert(loginPassword);
+
     var userData = {
 
         email: userLoginEmailId,
@@ -12,12 +11,44 @@
         url: "http://174.141.233.6/YuY/users/login",
         data: userData,
         success: function (data) {
-            debugger;
+
+            console.log(data);
+
+            if (data.ResponseData[0].UserID != 0) {
+                window.location.replace("home.html");
+            }
+            else {
+                alert("wrong user");
+            }
+        },
+        error: function (xhr) {
+            alert(xhr.responseText);
+        }
+    });
+}
+function ValidateEmail() {
+
+    var postData = {
+        email: $("#SignUpEmail").val()
+    };
+    $.ajax({
+        type: "GET",
+        //  url: "http://localhost:6269/users/checkuser",
+        url: "http://174.141.233.6/YuY/users/checkuser",
+        data: postData,
+        success: function (data) {
+
+            if (data.ResponseData != 1) {
+
+            }
+            else {
+                alert("already registered");
+            }
             console.log(data);
             //alert("success..." + data);
         },
         error: function (xhr) {
-            debugger;
+
             alert(xhr.responseText);
         }
     });
