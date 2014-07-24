0bb209272897d327d8fcb755d6f28957c8ae48e1
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-07-24 18:43:05 +0530

    editProfile  and seeProfile done

--- yuYAPP-html/js/UserProfile.js
+++ yuYAPP-html/js/UserProfile.js
@@ -1,1 +1,28 @@
-﻿
+﻿function GetUserProfile() {
+    var userData = {
+        userId: 13
+    };
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/users/GetProfile",
+        url: "http://174.141.233.6/YuY/users/GetProfile",
+        data: userData,
+        success: function (data) {
+          
+            console.log(data);
+
+            $("#SeeUserName").val(data.ResponseData.UserName);        
+            $("#SeefullName").val(data.ResponseData.FirstName + " " + data.ResponseData.LastName);
+            $("#SeeGender").val(data.ResponseData.Gender);
+            $("#SeeEmail").val(data.ResponseData.Email);
+            $("#SeeDOB").val(data.ResponseData.DOB);
+
+
+        },
+        error: function (xhr) {
+       
+            alert(xhr.responseText);
+        }
+    });
+}
+
---
+++ yuYAPP-html/js/updateProfile.js
@@ -0,0 +1,28 @@
+﻿function UpdateProfile(editFirstname, editLastname, editUserName, editDOB, editGender) {
+    alert(editGender);
+    var userData = {
+        userId: 13,
+        userName: editUserName,
+        password:'',
+        DOB: editDOB,
+        firstName: editFirstname,
+        lastName: editLastname,
+        gender: editGender,
+        securityQuestion: 'New question'
+    };
+    $.ajax({
+        type: "Post",
+     
+       url: "http://174.141.233.6/YuY/Users/UpdateProfile",
+        data: userData,
+        success: function (data) {
+           
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+          
+            alert(xhr.responseText);
+        }
+    });
+}
