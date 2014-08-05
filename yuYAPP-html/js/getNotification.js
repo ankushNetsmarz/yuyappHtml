01e5a6188b22ec5ec588f7a4d72c65be9f600b09
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-08-05 19:10:02 +0530

    getNotification

---
+++ yuYAPP-html/js/getNotification.js
@@ -0,0 +1,55 @@
+﻿function GetNotificationCount() {
+    var postData = {
+        userId: 1
+    }
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/posts/GetNotificationCount",
+        url: "http://174.141.233.6/YuY/posts/GetNotificationCount",
+        data: postData,
+        success: function (data) {
+            debugger;
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
+
+
+var userId = localStorage.getItem("userId");
+function GetNotification() {
+    var postData = {
+        userId: userId
+    }
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/posts/GetNotification",
+        url: "http://174.141.233.6/YuY/posts/GetNotification",
+        data: postData,
+        success: function (data) {
+
+            
+            var HTML = "";
+
+            if (data.ResponseData.length > 0) {
+
+                for (var i = 0; i < data.ResponseData.length; i++) {
+                    HTML+= "<div class='single-notifi'>"
+                    HTML+=  "<div class='fl notifi-by-user'><img src='images/user-pic-list.jpg' /></div>"
+                    HTML+=  "<div class='fl notifi-text'><span>Maninder Singh</span> updated this post"
+                    HTML+=  "<div class='notifi-msg-ago'>21 min</div>"
+                    HTML+=  "</div>"
+                    HTML+= "<div class='clr'></div>"
+                    HTML+=  "</div>"
+                }}
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
