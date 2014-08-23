e83ff37b4b2fba6d7b4e3d80b394ec8987916a0a
PoojaSharma2142 <pooja.sharma@weexcelindia.in>
2014-08-23 16:51:44 +0530

    yappLive

---
+++ yuYAPP-html/js/getImageVideoPost.js
@@ -0,0 +1,74 @@
+﻿
+var userId = localStorage.getItem("userId");
+
+function GetPostAnonymousList() {
+    var postData = {
+        userId: userId,
+        start: 1,
+        end: 10,
+        isall: false
+    }
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/posts/GetPostAnonymousList",
+       url: "http://174.141.233.6/YuY/posts/GetPostAnonymousList",
+        data: postData,
+        success: function (data) {
+            //debugger;
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
+
+/*get GetPostNonAnonymousList for Shhh */
+function GetPostNonAnonymousList() {
+    var postData = {
+        userId: userId,
+        start: 1,
+        end: 10,
+        isall: false
+    }
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/posts/GetPostNonAnonymousList",
+        url: "http://174.141.233.6/YuY/posts/GetPostNonAnonymousList",
+        data: postData,
+        success: function (data) {
+            //debugger;
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
+
+/*Set Unfriend*/
+function SetUnFriend() {
+    var postData = {
+        userId: userId,
+        friendId: 2
+    }
+    $.ajax({
+        type: "GET",
+        //url: "http://localhost:6269/posts/SetUnFriend",
+        url: "http://174.141.233.6/YuY/posts/SetUnFriend",
+        data: postData,
+        success: function (data) {
+            //debugger;
+            console.log(data);
+            //alert("success..." + data);
+        },
+        error: function (xhr) {
+            debugger;
+            alert(xhr.responseText);
+        }
+    });
+}
